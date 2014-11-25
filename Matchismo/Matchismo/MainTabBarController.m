27658cc72ceb840a9ed30d1aafde731e8703272b
Jakub Radlak <jradlak@gmail.com>
2014-11-25 21:30:27 +0100

    set icons for tabs in tabConttoller

---
+++ Matchismo/Matchismo/MainTabBarController.m
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.m
+//  Matchismo
+//
+//  Created by Jakub Radlak on 25.11.2014.
+//  Copyright (c) 2014 Jakub Radlak. All rights reserved.
+//
+
+#import "MainTabBarController.h"
+
+@interface MainTabBarController ()
+
+@end
+
+@implementation MainTabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ Matchismo/Matchismo/TabBarViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarViewViewController.h
+//  Matchismo
+//
+//  Created by Jakub Radlak on 25.11.2014.
+//  Copyright (c) 2014 Jakub Radlak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface TabBarViewViewController : ViewController
+
+@end
