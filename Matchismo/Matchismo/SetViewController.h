701c45c2a6d890ec45dbb02404cc61100ba44417
Jakub Radlak <jradlak@gmail.com>
2014-11-25 18:47:16 +0100

    adding tab view controller and second controller for a set game

---
+++ Matchismo/Matchismo/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  Matchismo
+//
+//  Created by Jakub Radlak on 25.11.2014.
+//  Copyright (c) 2014 Jakub Radlak. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SetViewController : ViewController
+
+@end
