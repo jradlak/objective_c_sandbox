2ff65c36d39e855471c160af039ad98cd247a755
Jakub Radlak <jradlak@gmail.com>
2014-11-29 22:07:27 +0100

    code refactoring, prepare to create model of set game

---
+++ Matchismo/Matchismo/SetCard.h
@@ -0,0 +1,13 @@
+//
+//  SetCard.h
+//  Matchismo
+//
+//  Created by Jakub Radlak on 29.11.2014.
+//  Copyright (c) 2014 Jakub Radlak. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+
+@end
---
+++ Matchismo/Matchismo/SetCard.m
@@ -0,0 +1,13 @@
+//
+//  SetCard.m
+//  Matchismo
+//
+//  Created by Jakub Radlak on 29.11.2014.
+//  Copyright (c) 2014 Jakub Radlak. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
+@end
